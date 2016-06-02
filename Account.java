66799a74ecef8721591b89cbb5250ccefd81e7c0
aravinda4044 <mishra.aravinda@gmail.com>
2016-06-02 08:52:56 +0530

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,17 @@
+
+public class Account {
+	private int accountNumber;
+	private double balance;
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+}
